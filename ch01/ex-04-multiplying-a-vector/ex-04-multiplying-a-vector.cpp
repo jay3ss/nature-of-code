459f486ef1c391319c4822f0483229a06d9f6e2c
jay ess <mentoce@gmail.com>
2018-08-17 22:39:23 -0700

    Create example 1.4

---
+++ ch01/ex-04-multiplying-a-vector/ex-04-multiplying-a-vector.cpp
@@ -0,0 +1,70 @@
+// Nature of Code  - Example 1.4 (Multiplying a vector)
+
+#include "SFML/Graphics.hpp"
+#include "jvector.cpp"
+
+double getMouseX(sf::RenderWindow*);
+double getMouseY(sf::RenderWindow*);
+
+int main()
+{
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "NoC - Example 1.4");
+
+    JVector center(WIDTH/2, HEIGHT/2);
+    JVector mouse(getMouseX(&window), getMouseY(&window));
+
+    sf::VertexArray line(sf::Lines, 2);
+    line[0].position = sf::Vector2f(center.x, center.y);
+    line[0].color = sf::Color::Green;
+    line[1].position = sf::Vector2f(center.x, center.y);
+    line[1].color = sf::Color::Red;
+
+    window.draw(line);
+    window.display();
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        mouse.x = getMouseX(&window);
+        mouse.y = getMouseY(&window);
+
+        mouse.sub(center);
+        mouse.mult(0.5);
+
+        // Create a transform object and set its position to be the center of
+        // the window. Then create a point that's translated from the center
+        // of the window to a point midway between the center of the window
+        // and the mouse.
+        sf::Transform translation;
+        translation.translate(WIDTH/2, HEIGHT/2);
+        sf::Vector2f endpoint = translation.transformPoint(mouse.x, mouse.y);
+
+        line[1].position = endpoint;
+        window.clear();
+        window.draw(line);
+        window.display();
+    }
+}
+
+
+double getMouseX(sf::RenderWindow *window)
+{
+    double x = sf::Mouse::getPosition(*window).x;
+    return x;
+}
+
+double getMouseY(sf::RenderWindow *window)
+{
+    double y = sf::Mouse::getPosition(*window).y;
+    return y;
+}
