73a3976fbfad27c191bbfa05eff302fa183a7dc4
jay ess <mentoce@gmail.com>
2018-08-18 00:34:58 -0700

    Create example 1.5

---
+++ ch01/ex-05-vector-magnitude/ex-05-vector-magnitude.cpp
@@ -0,0 +1,76 @@
+// Nature of Code - Example 1.3 (Vector subtraction)
+
+#include "SFML/Graphics.hpp"
+#include "jvector.cpp"
+
+double getMouseX(sf::RenderWindow &win);
+double getMouseY(sf::RenderWindow &win);
+
+
+int main()
+{
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "NoC - Example 1.3");
+
+    JVector center(WIDTH/2, HEIGHT/2);
+    JVector mouse(getMouseX(window), getMouseY(window));
+
+    // To draw a line, use the sf::VertexArray
+    sf::VertexArray line(sf::Lines, 2);
+    line[0].position = sf::Vector2f(center.x, center.y);
+    line[0].color = sf::Color::Green;
+    line[1].position = sf::Vector2f(center.x, center.y);
+    line[1].color = sf::Color::Red;
+
+    sf::RectangleShape magnitude(sf::Vector2f(0, 0));
+
+    window.draw(line);
+    window.display();
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        mouse.x = getMouseX(window);
+        mouse.y = getMouseY(window);
+        mouse.sub(center);
+    
+        sf::Transform translation;
+        translation.translate(WIDTH/2, HEIGHT/2);
+        sf::Vector2f endpoint = translation.transformPoint(mouse.x, mouse.y);
+
+        line[1].position = endpoint;
+
+        // Set the size of the rectangle (x-direction) based on the magnitude
+        // of the mouse vector
+        magnitude.setSize(sf::Vector2f(mouse.mag(), 20));
+
+        window.clear();
+        window.draw(magnitude);
+        window.draw(line);
+        window.display();
+    }
+
+    return 0;
+}
+
+
+double getMouseX(sf::RenderWindow &win)
+{
+    double x = sf::Mouse::getPosition(win).x;
+    return x;
+}
+
+double getMouseY(sf::RenderWindow &win)
+{
+    double y = sf::Mouse::getPosition(win).y;
+    return y;
+}
---
+++ ch01/ex-05-vector-magnitude/jvector.cpp
@@ -0,0 +1,52 @@
+#ifndef JVECTOR_EX_1_03_H
+#define JVECTOR_EX_1_03_H
+
+#include <cmath>
+
+class JVector{
+public:
+    double x;
+    double y;
+
+    JVector(double x_, double y_):
+        x(x_),
+        y(y_)
+        {}
+
+    void add(JVector);
+    void div(double);
+    double mag();
+    void mult(double);
+    void sub(JVector);
+};
+
+void JVector::add(JVector v)
+{
+    x += v.x;
+    y += v.y;
+}
+
+void JVector::div(double n)
+{
+    x /= n;
+    y /= n;
+}
+
+double JVector::mag()
+{
+    return std::sqrt(x*x + y*y);
+}
+
+void JVector::mult(double n)
+{
+    x *= n;
+    y *= n;
+}
+
+void JVector::sub(JVector v)
+{
+    x -= v.x;
+    y -= v.y;
+}
+
+#endif // JVECTOR_EX_1_03_H
