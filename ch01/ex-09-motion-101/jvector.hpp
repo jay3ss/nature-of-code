2739eb9e62c18e5ab5fa6c18575ce21b24aca263
jay ess <mentoce@gmail.com>
2018-08-18 20:39:01 -0700

    Create example 1.8

---
+++ ch01/ex-09-motion-101/jvector.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cmath>
+
+class JVector{
+public:
+    double x;
+    double y;
+
+    JVector(double x_, double y_):
+        x(x_),
+        y(y_)
+        {}
+
+    JVector():
+        x(0),
+        y(0)
+        {}
+    void add(JVector);
+    void div(double);
+    void limit(double);
+    double mag();
+    void mult(double);
+    void normalize();
+    void sub(JVector);
+};
+
+void JVector::add(JVector v)
+{
+    x += v.x;
+    y += v.y;
+}
+
+void JVector::div(double n)
+{
+    x /= n;
+    y /= n;
+}
+
+void JVector::limit(double lim)
+{
+    if(mag()*mag() > lim)
+    {
+        normalize();
+        mult(lim);
+    }
+}
+
+double JVector::mag()
+{
+    return std::sqrt(x*x + y*y);
+}
+
+void JVector::mult(double n)
+{
+    x *= n;
+    y *= n;
+}
+
+void JVector::normalize()
+{
+    float m = mag();
+    if(m != 0)
+    {
+        div(m);
+    }
+}
+
+void JVector::sub(JVector v)
+{
+    x -= v.x;
+    y -= v.y;
+}
